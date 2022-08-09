49794350c02a135d39d921dda8899b3b8c9d451a
LogicVersion <info@logicversion.ng>
2022-08-09 17:16:51 +0100

    090822V

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,14 @@
 export const environment = {
   production: true,
-  //appURL: 'http://localhost:8081/api',
+  // appURL: 'http://localhost:8081/api/clearing',
+  // appURL: 'https://localhost:7118/api/clearing',
   appURL: 'http://sapidholdingsonline.com/sapi/api/clearing', //billingAPI
-  //apiURL_Admin: 'http://localhost:8082/api',
-  apiURL_Admin: 'http://sapidholdingsonline.com/apix/api/authenticate', //AuthAPI
+  // apiURL_Admin: 'http://localhost:8082/api/authenticate', //http://localhost:5000/api',
+  // apiURL_Admin: 'http://localhost:5000/api/authenticate', //http://localhost:5000/api',
+  apiURL_Admin:'http://sapidholdingsonline.com/apix/api/authenticate', // /api/authenticate', //AuthAPI
   coyID: 'Clearing',
   coyName: 'Sapid Agencies Ltd',
   imgLogo: '',
   imgHome: 'assets/images/SapidInTheCloud2.png',
+  VAT: 7.5,
 };
